d098bad8fbd8b08a4a2c33d0c672a54b4446a82e
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-08-26 14:58:55 +0200

    export key and use mock

--- app/appConverter.js
+++ app/appConverter.js
@@ -6,9 +6,11 @@ var key = access_key.KEY.FIXER;
 // console.log(access_key.KEY.FIXER);
 
 module.exports.getLatestRates = function(callback){
-    request('http://data.fixer.io/api/latest?access_key='+key, 
+    request('http://data.fixer.io/api/latest?access_key=' + key, 
         function(error, reponse, body){
             var ratesList = JSON.parse(body);
             return callback(ratesList);
         });
 };
+
+exports.KEY =  key;
--- spec/appConverterSpec.js
+++ spec/appConverterSpec.js
@@ -1,8 +1,20 @@
 var app = require('../app/appConverter');
 var helpers = require('../app/helpers/helpers');
+var nock = require('nock');
+var api = require('../api');
+var KEY = app.KEY;
+
 describe('Currency Converter', function(){
 
     describe('GET echange rates', function(){
+
+        beforeEach(function(){
+            //Finished in 0.09 seconds vs 1.619 seconds without nock!!
+            var ratesResponse = api;
+            nock('http://data.fixer.io')
+                .get('/api/latest?access_key=' + KEY)
+                .reply(200, ratesResponse);
+        });
 
         it('should return object', function(done){
             app.getLatestRates(function(result){
@@ -21,7 +33,7 @@ describe('Currency Converter', function(){
         it('should return at least 1 rate', function(done){
             app.getLatestRates(function (result) {
                 var ratesCount = helpers.countKeys(result.rates);
-                expect(ratesCount).toBeGreaterThan(1);
+                expect(ratesCount).toBeGreaterThan(0);
                 done();
             });
         });
