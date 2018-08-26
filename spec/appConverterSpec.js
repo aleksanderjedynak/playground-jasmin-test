90fcb881d5fb5237e54933e4282accd4b447fb89
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-08-26 14:25:44 +0200

    new test to async

--- spec/appConverterSpec.js
+++ spec/appConverterSpec.js
@@ -1,4 +1,5 @@
 var app = require('../app/appConverter');
+var helpers = require('../app/helpers/helpers');
 describe('Currency Converter', function(){
 
     describe('GET echange rates', function(){
@@ -6,6 +7,29 @@ describe('Currency Converter', function(){
         it('should return object', function(done){
             app.getLatestRates(function(result){
                 expect(typeof result).toEqual('object');
+                done();
+            });
+        });
+
+        it('should return success!', function (done) {
+            app.getLatestRates(function (result) {
+                expect(result.success).toEqual(true);
+                done();
+            });
+        });
+
+        it('should return at least 1 rate', function(done){
+            app.getLatestRates(function (result) {
+                var ratesCount = helpers.countKeys(result.rates);
+                expect(ratesCount).toBeGreaterThan(1);
+                done();
+            });
+        });
+
+        it('should return at least all rates', function (done) {
+            app.getLatestRates(function (result) {
+                var ratesCount = helpers.countKeys(result.rates);
+                expect(ratesCount).toEqual(168);
                 done();
             });
         });
