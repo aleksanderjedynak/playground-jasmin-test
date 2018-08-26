0516aaa1e9c050d917dd49e616f672505c7bb706
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-08-26 14:25:06 +0200

    fix url to fixer.io

--- app/appConverter.js
+++ app/appConverter.js
@@ -6,7 +6,7 @@ var key = access_key.KEY.FIXER;
 // console.log(access_key.KEY.FIXER);
 
 module.exports.getLatestRates = function(callback){
-    request('http://data.fixer.io/api/latest?access_key=key', 
+    request('http://data.fixer.io/api/latest?access_key='+key, 
         function(error, reponse, body){
             var ratesList = JSON.parse(body);
             return callback(ratesList);
